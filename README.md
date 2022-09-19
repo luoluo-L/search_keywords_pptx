6634d3f2574a729b8f1fce13af566efa7a9626a0
luoluo <elydia.777@gmail.com>
2022-09-19 15:03:24 -0400

    add command line runing function

--- README.md
+++ README.md
@@ -2,4 +2,13 @@ search_keywords_from_pptx_slides.py: search a keyword from powerpoint slides fro
 
 Input: folder name, keyword.
 
-Note: keyword has to be the extact match.
+Note: keyword has to be the exact match.
+
+run code in the command line:
+
+```sh
+python search_keywords_from_pptx_slides.py PPTXFOLDER keyword
+
+```
+
+TODO: filter out files in the specified folder that are not ppt slides.
--- search_keywords_from_pptx_slides.py
+++ search_keywords_from_pptx_slides.py
@@ -1,15 +1,19 @@
 # search key words in slides
 
+import sys
+
+# To install the package, use the following command:
+# pip install python-pptx
+
+from pptx import Presentation
+import glob
+
 if __name__ == '__main__':
 
-    test_pptx_path = "Test Path with pptx slides"
+    #test_pptx_path = "Test Path with pptx slides"
+    test_pptx_path = sys.argv[1]
+    search_term = sys.argv[2] #'results'
 
-    #To install the package, use the following command:
-    #pip install python-pptx 
-    from pptx import Presentation
-    import glob
-
-    search_term = 'results'
     for eachfile in glob.glob(test_pptx_path):
         prs = Presentation(eachfile)
         #print(eachfile)
@@ -17,7 +21,6 @@ if __name__ == '__main__':
         for slide_num, slide in enumerate(prs.slides):
             for shape_num, shape in enumerate(slide.shapes):
                 if hasattr(shape, "text"):
-                    #print(shape.text)
 
                     if search_term in shape.text.lower():
                         print(('Slide filename: '+str(eachfile)+', slide number: ' + str(slide_num+1) +
